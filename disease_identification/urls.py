753d2fcb691a9177b21f43467b979ab6de4169b7
rashen <rashenrashen4@gmail.com>
2024-10-18 23:18:53 +0530

    add dises identfication

--- disease_identification/urls.py
+++ disease_identification/urls.py
@@ -1,8 +1,10 @@
 # disease_identification/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='disease_identification_index'),
-    # Add other paths here
+    path('disease-identification/', views.disease_identification, name='disease_identification_index'),
 ]
+
+
