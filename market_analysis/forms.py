f786e70456e90e0f8255aa153d41f0d9e803b0cd
rashen <rashenrashen4@gmail.com>
2025-01-30 23:22:20 +0530

    Your commit message

--- market_analysis/forms.py
+++ market_analysis/forms.py
@@ -1,11 +1,25 @@
 from django import forms
+import os
+import pandas as pd
+from django.conf import settings
+
+# Load the dataset and extract unique values
+dataset_path = os.path.join(settings.BASE_DIR, 'dataset', 'dataset.csv')
+df = pd.read_csv(dataset_path)
+
+admin1_choices = [(value, value) for value in df['admin1'].unique()]
+admin2_choices = [(value, value) for value in df['admin2'].unique()]
+market_choices = [(value, value) for value in df['market'].unique()]
+category_choices = [(value, value) for value in df['category'].unique()]
+commodity_choices = [(value, value) for value in df['commodity'].unique()]
+unit_choices = [(value, value) for value in df['unit'].unique()]
 
 class PricePredictionForm(forms.Form):
-    admin1 = forms.CharField(label='Admin1', max_length=100)
-    admin2 = forms.CharField(label='Admin2', max_length=100)
-    market = forms.CharField(label='Market', max_length=100)
-    category = forms.CharField(label='Category', max_length=100)
-    commodity = forms.CharField(label='Commodity', max_length=100)
-    unit = forms.CharField(label='Unit', max_length=50)
+    admin1 = forms.ChoiceField(label='Admin1', choices=admin1_choices)
+    admin2 = forms.ChoiceField(label='Admin2', choices=admin2_choices)
+    market = forms.ChoiceField(label='Market', choices=market_choices)
+    category = forms.ChoiceField(label='Category', choices=category_choices)
+    commodity = forms.ChoiceField(label='Commodity', choices=commodity_choices)
+    unit = forms.ChoiceField(label='Unit', choices=unit_choices)
     start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
