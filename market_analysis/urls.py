b415cf7a07d83df5707c7d818b20195d239c3dbb
rashen <rashenrashen4@gmail.com>
2025-01-31 01:39:47 +0530

    inrove

--- market_analysis/urls.py
+++ market_analysis/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.predict_price_view, name='market_analysis_index'),
     path('predict/', views.predict_price_view, name='predict_price'),
+  
 
 ]
