c1855ba8339619d6625dc83b1184fb7bbc5ca395
rashen <rashenrashen4@gmail.com>
2025-01-30 21:51:17 +0530

    intergate luxchana part with main system

--- market_analysis/urls.py
+++ market_analysis/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='market_analysis_index'),
+    path('', views.predict_price_view, name='market_analysis_index'),
+    path('predict/', views.predict_price_view, name='predict_price'),
+
 ]
